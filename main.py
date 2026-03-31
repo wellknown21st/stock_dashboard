814fcbcd77eb0b37718e2ea67088bb3dea73650f
Mayank Mishra <161496387+wellknown21st@users.noreply.github.com>
2026-03-31 16:38:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -607,4 +607,4 @@ def health_check(db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+   uvicorn main:app --host 0.0.0.0 --port $PORT
